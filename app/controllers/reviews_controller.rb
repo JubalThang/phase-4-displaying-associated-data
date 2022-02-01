e1b472dbb43a3fb23218cc5fce97d97867ba7ddc
JubalThang <jubalthang@gmail.com>
2022-02-01 10:53:03 -0600

    complete assignment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, only:[:username], include: {dog_house: {only: [:name, :city]}}
   end
 
 end
