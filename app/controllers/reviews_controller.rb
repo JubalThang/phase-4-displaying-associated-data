c849265924acb51da831a653ee298cfcffeb2543
JubalThang <jubalthang@gmail.com>
2022-02-01 12:44:01 -0600

    added nested routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,22 @@ class ReviewsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
-  def index
-    reviews = Review.all.order(rating: :desc)
-    render json: reviews, only:[:username], include: {dog_house: {only: [:name, :city]}}
+  # this index is simple GET with no nested route
+  # def index
+  #   reviews = Review.all.order(rating: :desc)
+  #   render json: reviews, only:[:username], include: {dog_house: {only: [:name, :city]}}
+  # end
+
+  # nested index route
+  def index 
+    if params[:dog_house_id] 
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else 
+      reviews = Review.all 
+    end
+
+    render json: reviews, include: :dog_house
   end
 
   def show 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :reviews, only: [:index, :destroy, :show]
-  resources :dog_houses, only: [:show, :index, :destroy]
+
+  resources :reviews, only: [:index, :destroy, :show] 
+
+  resources :dog_houses, only: [:show, :index, :destroy] do 
+    # nested resource for reviews
+    resources :reviews, only: [:show, :index]
+  end
+
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
