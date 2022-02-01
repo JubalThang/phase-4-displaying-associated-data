f06170e5f889547702a29b9e71e6b7b10775ca7a
JubalThang <jubalthang@gmail.com>
2022-02-01 12:04:35 -0600

    handle error not found for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,15 @@
 class ReviewsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   def index
     reviews = Review.all.order(rating: :desc)
     render json: reviews, only:[:username], include: {dog_house: {only: [:name, :city]}}
+  end
+
+  def show 
+    review = Review.find(params[:id])
+    render json: review, status: :ok
   end
 
   def detroy 
@@ -13,6 +20,10 @@ class ReviewsController < ApplicationController
     else
       render json: {errors: "The review is not found"} 
     end
+  end
+
+  def render_not_found
+    render json: {error: "No reviews"}, status: :not_found
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reviews, only: [:index, :destroy]
+  resources :reviews, only: [:index, :destroy, :show]
   resources :dog_houses, only: [:show, :index, :destroy]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
